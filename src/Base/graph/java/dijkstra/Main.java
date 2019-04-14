81f55c9faedeefd6f755ac58ffce589da8b8d709
suns1824 <1070082859@qq.com>
2019-04-15 03:19:43 +0800

    图论++

---
+++ src/Base/graph/java/dijkstra/Main.java
@@ -0,0 +1,13 @@
+package Base.graph.java.dijkstra;
+
+public class Main {
+    /*
+    Dijkstra算法：
+    V(v1,v2....vn)
+    S为known的，U为unknown的。
+    在所有unknown顶点中选择distance最小的vi，加入到S中，然后U中的节点的distance更新。
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Base/graph/java/mintree/Main.java
@@ -0,0 +1,19 @@
+package Base.graph.java.mintree;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    /*
+    Prim算法：
+    和Dijkstra算法几乎相同，只是距离更新处定义不同
+     */
+
+
+
+    /*
+    Kruskal算法：
+
+     */
+}
